814cab51d39396347001658fb0e3a8e483e7e3e5
Travis <travismay@gmail.com>
2014-10-21 15:32:36 -0500

    Create analysis file for workflow

---
+++ analysis.R
@@ -0,0 +1,15 @@
+download <- function() {
+  download.file("http://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv", "training.csv")
+  download.file("http://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv", "testing.csv")
+}
+
+read <- function(file) {
+  fread(file, na.strings=c("#DIV/0!",""))
+}
+
+build_report <- function() {
+  knit2html("project.Rmd", "index.html")
+}
+
+train <- read("training.csv")
+test <- read("testing.csv")
