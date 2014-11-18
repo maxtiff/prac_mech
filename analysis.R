f08695cf929cc50c35c84aacd50ed476d7d08d15
Travis <travismay@gmail.com>
2014-11-18 15:36:19 -0600

    Modify to use all 4 cores in processor

--- analysis.R
+++ analysis.R
@@ -6,21 +6,14 @@ library(doSNOW)
 required.scripts <- c('clean.R','error.R','filter.R','download.R','submisson.R')
 sapply(required.scripts, source, .GlobalEnv)
 
-cl <- makeCluster(2, type="SOCK")
+cl <- makeCluster(4, type="SOCK")
 registerDoSNOW(cl)
-
-
-
-read <- function(file) {
-  fread(file, na.strings=c("#DIV/0!",""), stringsAsFactors = F)
-}
-
-
 
 
 ## download files
 download()
 
+## Load files into environment
 trainRaw <- read("training.csv")
 testRaw <- read("testing.csv")
 
@@ -28,14 +21,6 @@ set.seed(1986)
 
 ## contains some NA values
 naCols <- trainRaw[,sapply(.SD, function(x) any(is.na(x)))]
-
-drop <- function(x) {
-  x[,!c("V1", "user_name", "raw_timestamp_part_1", "raw_timestamp_part_2", "cvtd_timestamp", "new_window", "num_window"),with=F]
-}
-
-transform.features <- function(x) {
-  x[,classe:=factor(classe)]
-}
 
 ## try only columns that have values
 trainFeatures <- drop(trainRaw[,eval(names(which(na.cols == F))),with=F])
@@ -45,6 +30,8 @@ answers <- predictions
 # setwd('~/parc_mech/files')
 # setwd('~/datasciencecoursera/parc_mech/files')
 
+## Write files for submission
 pml_write_files(predictions)
 
+## Write report for submission
 buildReport()
