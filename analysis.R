b7a8bbc3708a59dd946f96cb2ffaaa04f0a77396
Travis <travismay@gmail.com>
2014-11-18 13:53:50 -0600

    Modify analysis to use parallel processing

--- analysis.R
+++ analysis.R
@@ -1,18 +1,23 @@
+library(caret)
+library(foreach)
+library(doParallel)
+
 download <- function() {
   download.file("http://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv", "training.csv")
   download.file("http://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv", "testing.csv")
 }
 
 read <- function(file) {
-  fread(file, na.strings=c("#DIV/0!",""))
+  fread(file, na.strings=c("#DIV/0!",""), stringsAsFactors = F)
 }
+
 
 build_report <- function() {
   knit2html("project.Rmd", "index.html")
 }
 
-train <- read("training.csv")
-test <- read("testing.csv")
+trainRaw <- read("training.csv")
+testRaw <- read("testing.csv")
 
 set.seed(86)
 
