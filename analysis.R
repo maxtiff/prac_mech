4f466b0a03b7993d4c6b159bcf5db3d2004c741f
Travis <travismay@gmail.com>
2014-11-18 14:32:02 -0600

    Add cleaning functions to separate script

--- analysis.R
+++ analysis.R
@@ -2,7 +2,11 @@ library(caret)
 library(foreach)
 library(doSNOW)
 
-cl <- makeCluster(3, type="SOCK")
+## Load required scripts from workflow
+required.scripts <- c('clean.R','error.R','filter.R')
+sapply(required.scripts, source, .GlobalEnv)
+
+cl <- makeCluster(2, type="SOCK")
 registerDoSNOW(cl)
 
 download <- function() {
@@ -19,9 +23,34 @@ build_report <- function() {
   knit2html("project.Rmd", "index.html")
 }
 
+download()
+
 trainRaw <- read("training.csv")
 testRaw <- read("testing.csv")
 
-set.seed(86)
+set.seed(1986)
 
+## contains some NA values
+naCols <- trainRaw[,sapply(.SD, function(x) any(is.na(x)))]
 
+drop <- function(x) {
+  x[,!c("V1", "user_name", "raw_timestamp_part_1", "raw_timestamp_part_2", "cvtd_timestamp", "new_window", "num_window"),with=F]
+}
+
+transform.features <- function(x) {
+  x[,classe:=factor(classe)]
+}
+
+## try only columns that have values
+trainFeatures <- drop(trainRaw[,eval(names(which(na.cols == F))),with=F])
+
+# setwd('~/parc_mech/files')
+# setwd('~/datasciencecoursera/parc_mech/files')
+
+pml_write_files = function(x){
+  n = length(x)
+  for(i in 1:n){
+    filename = paste0("problem_id_",i,".txt")
+    write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+  }
+}
