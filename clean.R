6582b4a178ad2b41562d6c1155757ae92bc6cc40
Travis <travismay@gmail.com>
2014-11-21 10:48:23 -0600

    Modify filter function with validation

--- clean.R
+++ clean.R
@@ -3,15 +3,30 @@ drop <- function(x) {
 }
 
 transformFeatures <- function(x) {
-  x[,classe:=factor(classe)]
+  return(x[,classe:=factor(classe)])
 }
 
 filter <- function(data) {
-
+	# Remove NA values
   keep <- !sapply(data,function(x) any(is.na()))
-
   data <- data[,keep]
 
+  # Drop unnecessary columns
+  data <- drop(data)
 
+  # Convert name of data frame into string to test for features in data.
+  frameName <- deparse(substitute(data))
 
+  # Transform training-data-specific "classe" column into factor class of variable.
+  if (grepl("^(train)",frameName) == TRUE) {
+  	if ("classe" in colnames(data)) {
+  		transformFeatures(data)
+  	}
+  } else if (grepl("^(test)", frameName) == TRUE) {
+  	#continue
+  } else {
+  	print("Check that the names of your data frames are correctly labeled.")
+  }
+
+  return(data)
 }
