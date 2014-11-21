58c91c7b57172fa21ec25c6c8cea054f8abe7ac1
Travis <travismay@gmail.com>
2014-11-21 09:19:00 -0600

    Modify analysis with new, modified cleaning functionals

--- analysis.R
+++ analysis.R
@@ -1,9 +1,10 @@
 library(caret)
 library(foreach)
 library(doSNOW)
+library(data.table)
 
 ## Load required scripts from workflow
-required.scripts <- c('clean.R','error.R','filter.R','download.R','submisson.R')
+required.scripts <- c('clean.R','error.R','download.R','submission.R')
 sapply(required.scripts, source, .GlobalEnv)
 
 cl <- makeCluster(4, type="SOCK")
@@ -20,7 +21,7 @@ testRaw <- read("testing.csv")
 set.seed(1986)
 
 ## contains some NA values
-naCols <- trainRaw[,sapply(.SD, function(x) any(is.na(x)))]
+trainClean <- filter(trainRaw) # [,sapply(.SD, function(x) any(is.na(x)))]
 
 ## try only columns that have values
 trainFeatures <- drop(trainRaw[,eval(names(which(na.cols == F))),with=F])
--- clean.R
+++ clean.R
@@ -1,7 +1,17 @@
 drop <- function(x) {
-  x[,!c("V1", "user_name", "raw_timestamp_part_1", "raw_timestamp_part_2", "cvtd_timestamp", "new_window", "num_window"),with=F]
+  return(x[,!c("V1", "user_name", "raw_timestamp_part_1", "raw_timestamp_part_2", "cvtd_timestamp", "new_window", "num_window"),with=F])
 }
 
 transformFeatures <- function(x) {
   x[,classe:=factor(classe)]
+}
+
+filter <- function(data) {
+
+  keep <- !sapply(data,function(x) any(is.na()))
+
+  data <- data[,keep]
+
+
+
 }
